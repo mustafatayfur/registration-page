225e50cf5ea74eae072aa5fca172f2ffce7e7f18
mustafa <mltm0204@gmail.com>
2021-11-16 21:17:31 +0300

    js codes are added

---
+++ script.js
@@ -0,0 +1,10 @@
+const _name = document.getElementById("name");
+const _path = document.getElementById("path");
+const _url = document.querySelector("#url");
+const addButton = document.getElementById("registration-form").lastElementChild;
+
+const clearStudentsBtn = document.querySelector("#clear-students");
+const tbody = document.querySelector("#Students");
+const loading = document.querySelector("#loading");
+const table = document.querySelector(".table");
+
