70edbefb76c6759e72e42adc6ec50c91a7c78884
mustafa <mltm0204@gmail.com>
2021-11-16 21:30:24 +0300

    studen.js codes are added

--- student.js
+++ student.js
@@ -1,1 +1,14 @@
-// class student
+// class Student1{
+//     constructorStudent(imageUrl, studentName, studentPath){
+//         console.log(this);
+//     }
+// }
+function Student(imageUrl, studentName, studentPath){
+    console.log(this);
+    console.log(self);
+    this.imageUrl = imageUrl;
+    this.studentName = studentName;
+    this.studentPath = studentPath;
+    console.log(this);
+}
+
